4ba2135cc83da9a3d9062e62e28d2265f07078f3
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-11-16 17:07:12 +0330

    the problem of showing members fixed

--- src/components/OneGroupPage/OneGroupPage.js
+++ src/components/OneGroupPage/OneGroupPage.js
@@ -23,15 +23,11 @@ const OneGroupPage = () => {
     };
     getContacts();
 
-    const savedMembers = JSON.parse(localStorage.getItem("members")) || [];
-    setMembers(savedMembers);
+    const savedGroups = JSON.parse(localStorage.getItem("groups"));
+    const membersOfGroup = savedGroups.find((g) => g.id === parseInt(id)).member;
+    setMembers(membersOfGroup);
+    
   }, []);
-
-  useEffect(() => {
-    if (members.length > 0) {
-      localStorage.setItem("members", JSON.stringify(members));
-    }
-  }, [members]);
 
   const addMemberHandler = (contactId) => {
     const index = groups.findIndex((item) => item.id === parseInt(id));
@@ -41,7 +37,9 @@ const OneGroupPage = () => {
     const updatedGroups = [...groups];
     updatedGroups[index] = group;
     setGroups(updatedGroups);
-    setMembers(group.member);
+    const membersOfGroup = groups.find((g) => g.id === parseInt(id)).member;
+    setMembers(membersOfGroup);
+    
   };
 
   const removeMember = (memberId) => {
@@ -51,9 +49,8 @@ const OneGroupPage = () => {
     const updatedGroups = [...groups];
     updatedGroups[index] = group;
     setGroups(updatedGroups);
-    const filteredMembers= members.filter((m)=>m.id !== memberId);
+    const filteredMembers = members.filter((m) => m.id !== memberId);
     setMembers(filteredMembers);
-    localStorage.removeItem("members")
   };
 
   return (
@@ -69,6 +66,7 @@ const OneGroupPage = () => {
         modalIsShow={modalIsShow}
         setModalIsShow={setModalIsShow}
         addMemberHandler={addMemberHandler}
+        removeMember={removeMember}
         id={id}
       />
 
@@ -85,32 +83,36 @@ const OneGroupPage = () => {
       </div>
 
       <div className="max-h-[65vh] overflow-auto">
-        {members
-          ? members.map((member) => {
-              return (
-                <div key={member.id} className="flex items-center mb-3 ">
-                  <div className="bg-indigo-100 w-14 h-14 rounded-full flex justify-center items-center mr-2 uppercase font-bold text-slate-600 text-2xl">
-                    {member.name.substr(0, 1)}
-                  </div>
-                  <div className="border-b-2 w-full pb-4 flex justify-between items-center">
-                    <div>
-                      <div className="font-bold uppercase mb-2">
-                        {member.name}{" "}
-                      </div>
-                      <div>Email : {member.email}</div>
-                      <div>Phone Number : {member.phone}</div>
-                    </div>
-                    <button
-                      className="border  border-indigo-400 p-1 rounded-md text-indigo-400 font-bold"
-                      onClick={() => removeMember(member.id)}
-                    >
-                      Remove
-                    </button>
-                  </div>
+        {members.length ? (
+          members.map((member) => {
+            return (
+              <div key={member.id} className="flex items-center mb-3 ">
+                <div className="bg-indigo-100 w-14 h-14 rounded-full flex justify-center items-center mr-2 uppercase font-bold text-slate-600 text-2xl">
+                  {member.name.substr(0, 1)}
                 </div>
-              );
-            })
-          : ""}
+                <div className="border-b-2 w-full pb-4 flex justify-between items-center">
+                  <div>
+                    <div className="font-bold uppercase mb-2">
+                      {member.name}{" "}
+                    </div>
+                    <div>Email : {member.email}</div>
+                    <div>Phone Number : {member.phone}</div>
+                  </div>
+                  <button
+                    className="border-2  border-indigo-400 p-1 rounded-md text-indigo-400 font-bold text-sm"
+                    onClick={() => removeMember(member.id)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p className="mt-16 text-center w-full">
+            There is no member in this group
+          </p>
+        )}
       </div>
     </section>
   );
